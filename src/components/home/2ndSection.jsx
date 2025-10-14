13910d878ad6a486240eb771688805c0353e3f3e
Touheed0 <tuheedahmad360@gmail.com>
2025-10-13 23:19:53 -0700

    Replace twitter link to facebook

--- src/components/home/2ndSection.jsx
+++ src/components/home/2ndSection.jsx
@@ -4,7 +4,7 @@ import DPImage from "../../assets/avatar.svg";
 
 //Material Icons
 import GitHubIcon from '@mui/icons-material/GitHub';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
@@ -98,7 +98,7 @@ const SecondSection = () => {
 
                     <SocialIconsBox>
                         <Link to="https://github.com/Touheed0" target="blank"><GitHubIcon /></Link>
-                        <Link to=""><TwitterIcon /></Link>
+                        <Link to="https://www.facebook.com/tuheed.ahmad.9421450" target="blank"><FacebookIcon /></Link>
                         <Link to=""><LinkedInIcon /></Link>
                         <Link to="https://www.instagram.com/touheed____ahmad/" target="blank"><InstagramIcon /></Link>
                     </SocialIconsBox>
