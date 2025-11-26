0705a40a267f515acd53571c19fee72db5526585
Touheed0 <tuheedahmad360@gmail.com>
2025-11-26 08:47:59 -0800

    Adding New Skills

--- src/utils/skills.js
+++ src/utils/skills.js
@@ -1,14 +1,16 @@
-import { FaReact, FaNode , FaGitAlt , FaJs } from "react-icons/fa";
-import { SiExpress, SiMongodb, SiVite } from "react-icons/si";
+import { FaReact, FaNode, FaGitAlt, FaJs } from "react-icons/fa";
+import { SiExpress, SiMongodb, SiVite, SiTailwindcss, SiSupabase } from "react-icons/si";
 
 const skillsData = [
     { id: 1, icon: FaJs },        // JavaScript
     { id: 2, icon: FaReact },     // React.js
     { id: 3, icon: SiVite },      // React Vite
-    { id: 4, icon: FaNode  },    // Node.js
+    { id: 4, icon: FaNode },    // Node.js
     { id: 5, icon: SiExpress },   // Express.js
     { id: 6, icon: SiMongodb },   // MongoDB
-    { id: 7, icon: FaGitAlt  },    // Git
+    { id: 7, icon: FaGitAlt },    // Git
+    { id: 8, icon: SiTailwindcss },    // Tailwind Css
+    { id: 11, icon: SiSupabase },    // Supabase
 ];
 
 export default skillsData;
